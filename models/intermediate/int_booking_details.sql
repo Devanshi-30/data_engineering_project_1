4c55dabac6d353e920e7d7413ce4bd20633e8e03
Devanshi Agarwal <devanshiec1034@gmail.com>
2024-11-18 15:08:34 +0000

    updated file

--- models/intermediate/int_booking_details.sql
+++ models/intermediate/int_booking_details.sql
@@ -5,11 +5,20 @@
         materialized='incremental',  
         unique_key='booking_id', 
         incremental_strategy='merge', 
-        updated_at='updated_at' 
-       
+        updated_at='updated_at',
+        pre_hook=[
+            "insert into TRAVEL_DB.dbt_dagarwal.model_run_log (model_name, run_time, status) values ('int_booking_details', current_timestamp(), 'start')"
+        ],
+        post_hook=[
+            "insert into TRAVEL_DB.dbt_dagarwal.model_run_log (model_name, run_time, status) values ('int_booking_details', current_timestamp(), 'success')"
+        ]
          
     )
+
+    
   }}
+
+  
 
 
 -- SQL for the model
@@ -18,25 +27,22 @@ with booking as (
         b.booking_id as booking_id,
         b.customer_id,
         b.destination_type,
+        d.description as destination_type_description,
         b.booking_date,
         b.booking_time,
         b.amount_spent,
         b.currency_code,
         b.status,
         b.segment_id,
-        co.country_name,
-        d.description as destination_type_description,
         s.segment_name as customer_segment_name,
+        co.country_name,
         b.booking_time as updated_at, 
         case 
-            when b.currency_code = 'USD' then b.amount_spent
             when b.currency_code = 'EUR' then b.amount_spent / coalesce(dur.exchange_rate_to_usd, 1)
             when b.currency_code = 'CAD' then b.amount_spent / coalesce(dur.exchange_rate_to_usd, 1)
             when b.currency_code = 'INR' then b.amount_spent / coalesce(dur.exchange_rate_to_usd, 1)
-            when b.currency_code = 'GBP' then b.amount_spent / coalesce(dur.exchange_rate_to_usd, 1)
-            else b.amount_spent -- if it's already USD, no conversion
+            else b.amount_spent 
         end as amount_spent_usd,
-        co.country_name as country_name
     from 
         {{ ref('stg_booking_details') }} b
     left join 
--- models/marts/fct_booking_summary.sql
+++ models/marts/fct_booking_summary.sql
@@ -2,21 +2,20 @@
 
 with booking_summary as (
     select
-        b.booking_id,
         b.destination_type,  -- Dimension: Destination Type
         b.destination_type_description,
-        b.customer_segment_name,  -- Dimension: Customer Segment
         b.country_name,  -- Dimension: Country
-        sum(b.amount_spent_usd) as total_amount_spent,  -- Measure
+        round(sum(b.amount_spent_usd),2) as total_amount_spent,  -- Measure
         count(distinct b.booking_id) as total_bookings  -- Measure
     from 
         {{ ref('int_booking_details') }} b
     group by 
-        b.booking_id,
         b.destination_type,
         b.destination_type_description,
-        b.customer_segment_name,
         b.country_name
+    order by 
+      total_amount_spent desc
+        
 )
 
 select * from booking_summary
